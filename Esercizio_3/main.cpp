e0b471fccd41ad5536c28dcb4dd2f5593be23272
nicolovinci <12999303@itisrossi.vi.it>
2026-02-11 11:36:32 +0100

    Need to finish Exercise 3

---
+++ Esercizio_3/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<string> cats = {
+        "Milo",
+        "Luna",
+        "Oscar",
+        "Mimi",
+        "Nero",
+        "Mango",
+        "Toby",
+        "Moka"
+    };
+    vector<string> cats2 = {
+        "Milo",
+        "Zoe",
+        "Max",
+        "Pippo",
+        "Mirtillo",
+        "Ruby",
+        "Mina",
+        "Leo",
+        "Muffin",
+        "Chicco"
+    };
+    while (1)
+    {
+        for (int i = 0; i < cats.size(); i++)
+        {
+
+        }
+    }
+    return 0;
+}
